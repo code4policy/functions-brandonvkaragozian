eaccc384cf694edbb9bd75f3cf1b92d27ef9d04b
Brandon Karagozian <brandonvkaragozian@gmail.com>
2026-01-14 00:37:02 -0500

    add prints and descriptions

--- calculator.py
+++ calculator.py
@@ -1,16 +1,30 @@
-def multiply(a,b):
+#multiplication function
+def multiply(a, b):
     return a * b
-
-def add(a,b):
-	return a+b
-
-def subtract(a,b):
-	return a-b
-
+#addition function 
+def add(a, b):
+    return a + b
+    
+#subtraction function
+def subtract(a, b):
+    return a - b
+    
+#division function
 def divide(a,b):
-	return a/b
+    return a/b
 
-
-print("I'm going use the calculator functions to multiply 5 and 6")
-x = multiply(5,6)
+print("I'm going use the calculator functions to multiply 7 and 8")
+x = multiply(7,8)
 print(x)
+
+print("I am going to use the calculator to add 13 and 48")
+y = add(13,48)
+print(y)
+
+print("I am going to use the calculator to subract 988 from 2050")
+s = subtract(2050,988)
+print(s)
+
+print("I am finally going to use the calculator to divide 100 by 4")
+d = divide(100,4)
+print(d)
